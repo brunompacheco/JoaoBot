10ef8fa39bc6bdaa69931c7f5d07a0145830e0f9
Bruno M. Pacheco <brunomachado-2005@hotmail.com>
2016-02-18 15:46:26 -0200

    PI-fucking-D

--- JoaoBot.c
+++ JoaoBot.c
@@ -8,6 +8,7 @@
 #include "l3g4200d.h"
 #include "adxl345.h"
 #include "Motor.h"
+#include "PID_v1.h"
 
 #define SAD 0x69<<1
 
@@ -18,9 +19,15 @@ double gx = 0;
 double gy = 0;
 double gz = 0;
 
-volatile double angle = 0;
+volatile FloatType angle = 0;
+
+volatile FloatType outmot = 0;
+
+volatile FloatType setpoint = 0;
 
 ISR(TIMER0_OVF_vect);
+
+PidType PID;
 
 int main() {
 	motorInit();
@@ -30,7 +37,7 @@ int main() {
 	usartEnableTransmitter();
 	usartStdio();
 
-	timer0ClockPrescaller64();
+	timer0ClockPrescaller256();
 	timer0NormalMode();
 	timer0OC0AOff();
 	timer0OC0BOff();
@@ -44,6 +51,12 @@ int main() {
 	l3g4200d_init();
 	l3g4200d_setoffset(0.11, -1.71, -0.46);
 	//l3g4200d_settemperatureref();
+
+	PID_init(&PID, 5,0,0, 0);
+	PID_SetMode(&PID, 1);
+	PID_SetOutputLimits(&PID, -255, 255);
+
+	PID.mySetpoint = 0;
 
 	_delay_ms(200);
 
@@ -67,11 +80,14 @@ ISR(TIMER0_OVF_vect) {
 	l3g4200d_getdata(&gx,&gy,&gz);
 	adxl345_getdata(&ax, &ay, &az);
 
-//	angle = (0.991875)*(angle + gy*0.004096) + (0.008125)*((az*180)/3.14159);
-	angle = (0.98)*(angle + gy*0.01) + (0.02)*((az*180)/3.14159);
+	angle = (0.96)*(angle + gy*0.004096) + (0.04)*((az*180)/3.14159);
+
+	PID.myInput = angle;
+	PID_Compute(&PID);
 
 	printf("\e[1;1H\e[2J");
 	printf("FUCKING ANGLE: %f\r\n", angle);
+	printf("PID output: %f\r\n", PID.myOutput);
 	printf("GX: %.2f\t|| AX: %.2f\r\n", gx, ax);
 	printf("GY: %.2f\t|| AY: %.2f\r\n", gy, ay);
 	printf("GZ: %.2f\t|| AZ: %.2f\r\n", gz, az);
